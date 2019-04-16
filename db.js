bb10fefbc971e4f4a082c8b73c160a2d69240060
Jørgen Jacobsen <jorgeja@gmail.com>
2019-04-16 13:57:02 +0200

    correct url

--- db.js
+++ db.js
@@ -5,27 +5,29 @@ mongoose.connect(
     process.env.DBUSER +
     ":" +
     process.env.DBPASSWORD +
-    "@ds123753.mlab.com:23753/kode24-julebase"
+    "@ds119606.mlab.com:19606/kode24-paaskebase"
 );
 
 const UserSchema = new mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  email: { type: String, unique: true, lowercase: true},
+  email: { type: String, unique: true, lowercase: true },
   username: { type: String, unique: true, lowercase: true },
   aggregatedAnswerCount: { type: Number, default: 0 },
-  answersInFolders: [{ type: mongoose.Schema.Types.ObjectId, ref: "Folder" }]
+  answersInFolders: [
+    { type: mongoose.Schema.Types.ObjectId, ref: "FileSystemUser" }
+  ]
+});
+
+const FileSystemUserSchema = new mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  username: { type: String, unique: true, lowercase: true },
+  password: { type: String }
 });
 
 const FolderSchema = new mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  fullpath: { type: String },
-  parent: { type: String },
-  name: { type: String },
-  availableFrom: { type: Date, default: Date.now },
-  passphrase: { type: String },
-  points: { type: Number, default: 1 },
-	answers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-	global: { type: Boolean },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "FileSystemUser" },
+  answers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
   files: [
     {
       name: { type: String },
@@ -37,27 +39,36 @@ const FolderSchema = new mongoose.Schema({
   ]
 });
 
+const FileSystemUser = mongoose.model("FileSystemUser", FileSystemUserSchema);
 const Folder = mongoose.model("Folder", FolderSchema);
 const User = mongoose.model("User", UserSchema);
 
 async function getPublicUsersByScore() {
   try {
-    return await User.find({}).select('-_id username aggregatedAnswerCount').sort("-aggregatedAnswerCount").exec();
+    return await User.find({})
+      .select("-_id username aggregatedAnswerCount")
+      .sort("-aggregatedAnswerCount")
+      .exec();
   } catch (error) {
-		console.log('dataerror',error);
+    console.log("dataerror", error);
     return false;
   }
 }
 
 async function getPublicUsersScoreByFolder() {
   try {
-    return await Folder.find({passphrase: { $exists: true}, "answers.0": { "$exists": true }}).select('-_id name').sort({"answers.length": -1}).populate("answers", "-_id username").exec();
+    return await Folder.find({
+      "answers.0": { $exists: true }
+    })
+      .select("-_id name")
+      .sort({ "answers.length": -1 })
+      .populate("answers", "-_id username")
+      .exec();
   } catch (error) {
-		console.log('dataerror',error);
+    console.log("dataerror", error);
     return false;
   }
 }
-
 
 module.exports = {
   getPublicUsersByScore: getPublicUsersByScore,
--- server.js
+++ server.js
@@ -1,50 +1,44 @@
-
-if (process.env.NODE_ENV !== 'production') {
-	require('dotenv').load();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").load();
 }
 
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser')
-const moment = require("moment")
-const cors = require("cors")
-const files = require('./files.js')
-const db = require('./db.js');
-const app = express()
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const moment = require("moment");
+const cors = require("cors");
+const files = require("./files.js");
+const db = require("./db.js");
+const app = express();
 
 console.log(new Date());
 
-
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client')))
+app.use(express.static(path.join(__dirname, "client")));
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(cookieParser())
+app.use(cookieParser());
 
-app.use(cors())
- 
+app.use(cors());
+
 // parse application/json
 app.use(bodyParser.json());
 
-
-app.get('/api/score/folder', cors(), async (req, res) => {
+app.get("/api/score/folder", cors(), async (req, res) => {
   let users = await db.getPublicUsersScoreByFolder();
   res.send(users);
 });
 
-app.get('/api/score/',cors(), async (req, res) => {
+app.get("/api/score/", cors(), async (req, res) => {
   let users = await db.getPublicUsersByScore();
   res.send(users);
 });
 
-
-
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/index.html'));
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/index.html"));
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port);
